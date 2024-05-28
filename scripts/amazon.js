71df16190e838bcaa36c7741ed028db71c756731
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-05-28 12:01:23 +0100

    Refactor cart quantity display logic

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,6 +77,16 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
           quantity: 1
         });
     }
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+
+    console.log(cartQuantity);
     console.log(cart);
     // MARK: Add to cart logic on top. 
   });
